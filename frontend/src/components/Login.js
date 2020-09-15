2a31545c58ffaa999bf05701de219a1b9dca9175
MMendes24 <mmbusiness1@protonmail.com>
2020-09-15 18:14:13 -0400

    Implemented a React client as per the stretch goal.

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axiosWithAuth from '../utils/axiosWithAuth'
 
+const initialCredentials = {
+    "username": "",
+    "password": "",
+}
+
 const Login = (props) => {
-    return(
-        <div>Test div!</div>
+    const [credentials, setCredentials] = useState(initialCredentials)
+
+    const onInputChange = e => {
+        setCredentials({
+            ...credentials,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+        axiosWithAuth()
+            .post("/auth/login", credentials)
+            .then(res => {
+                localStorage.setItem("token", res.data.token);
+            })
+            .catch(err => {
+                console.log(err, "You fool! You absolute buffoon!")
+            })
+        setCredentials(credentials)
+    }
+
+    return (
+        <form onSubmit={onSubmit}>
+            <label>Username:&nbsp;
+            <input
+                    name='username'
+                    type='text'
+                    value={credentials.username}
+                    onChange={onInputChange}
+                />
+            </label>
+
+            <label>Password:&nbsp;
+            <input
+                    name='password'
+                    type='text'
+                    value={credentials.password}
+                    onChange={onInputChange}
+                />
+            </label>
+            <button>Submit</button>
+        </form>
     )
 }
 
---
+++ frontend/src/components/Users.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import axiosWithAuth from '../utils/axiosWithAuth'
+
+const Users = (props) => {
+
+    return (
+        <section>
+            Hey!
+        </section>
+    )
+}
+
+export default Users
